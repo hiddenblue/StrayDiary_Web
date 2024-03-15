8d59e3901fd7aa2be6fffe67e2b88a07e60c1ad1
QiuLiang-99 <1297829693@qq.com>
2024-03-15 20:31:00 +0800

    no bug2

--- static/src/project.js
+++ static/src/project.js
@@ -14204,23 +14204,27 @@ require = function e(t, n, a) {
                     }
                 },
                 onLoad: function () {
-                    var t = ["一般", "快", "一般"], n = cc.find("Canvas/button/speed"), a = e("scr_data"), i = e("scr_effect"),
-                        speedtext = cc.find("Canvas/text/speed").getComponent("cc.Label").string = "你目前剧情（部分）播放速度为" + t[a.publicVar[6] || 2];
-
-                    let ex = cc.instantiate(n);//复制一个一模一样的按钮
-                    ex.setPosition(0, 459 - 150);//设置位置 往第一个按钮的下方移动150像素
-                    cc.find("Canvas/button").addChild(ex);  // 将按钮节点作为当前节点的子节点
-                    ex.getChildByName("New Label").getComponent("cc.Label").string = "探 索";
-                    //获得按钮上的节点，用名字搜索子节点       子节点上有一个组件    修改组件的字符 
+                    var t = ["一般", "快", "一般"],
+                        n = cc.find("Canvas/button/speed"), a = e("scr_data"), i = e("scr_effect"),
+                        speedtext = cc.find("Canvas/text/speed");
+                    speedtext.getComponent("cc.Label").string = "你目前剧情（部分）播放速度为" + t[a.publicVar[6] || 2];
+                    let ex = cc.instantiate(n);//复制一个一模一样的按钮 1
+                    ex.setPosition(0, 459 - 200);//设置位置 往第一个按钮的下方移动200像素 2
+                    cc.find("Canvas/button").addChild(ex);  // 将按钮节点作为当前节点的子节点 3
+                    ex.getChildByName("New Label").getComponent("cc.Label").string = "倍 率";
+                    //获得按钮上的节点，用名字搜索子节点       子节点上有一个组件    修改组件的字符  4
                     ex.color = cc.Color.RED;  //设置新节点的颜色
                     //text.string = "测试按钮";//text.parent = ex;//text.color = cc.Color.RED;
                     let exT = cc.instantiate(speedtext);//复制显示文字的label
-
-                    n.on("touchstart", function () {//添加触摸函数要最后加，不然复制的按钮也会有这个函数
+                    exT.setPosition(0, 557 - 200);// 如法炮制
+                    cc.find("Canvas/text").addChild(exT);
+                    exT.getComponent("cc.Label").string = "你当前拥有1x前进/探索速度\n（探索时会消耗对应倍数的精力\n但是奖励总量不变）";
+                    n.on("touchstart", function () {//添加触摸函数要最后加，不然复制的按钮也会有这个函数（也许吧）
                         a.publicVar[6] += 1;
                         a.publicVar[6] > 2 && (a.publicVar[6] = 1);
                         i.playText("Canvas/text/speed", "你目前剧情播放速度为" + t[a.publicVar[6]], 60);
                     }, n);
+
                 }
             });
             cc._RF.pop();
