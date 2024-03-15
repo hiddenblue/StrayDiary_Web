3f81cd546baa91025ff4650bc3f8ff1687799cca
QiuLiang-99 <125732151+QiuLiang-99@users.noreply.github.com>
2024-03-15 20:43:19 +0800

    Dev (#3)
    * bug appear
    
    * no bug
    
    * no bug2
    
    * no3
    
    * no4
    
    * bugfixed

--- static/src/project.js
+++ static/src/project.js
@@ -7767,7 +7767,7 @@ require = function e(t, n, a) {
                 properties: {},
                 onLoad: function () {
                     var t = cc.find("Canvas/Button/button_forward").getComponent("scr_forwardButton").constructor, n = new t(), a = new t(), i = new t(), c = new t(),//todo
-                        o = this, r = e("scr_data"),Tokyo = new t();
+                        o = this, r = e("scr_data"), Tokyo = new t();
                     this.node.runAction(cc.tintTo(.1, 255, 255, 255));//变成黑色
                     n.addDistance = function () { };
                     a.addDistance = function () { };
@@ -14192,20 +14192,43 @@ require = function e(t, n, a) {
             });
             cc._RF.pop();
         }, {}],
-        scr_system: [function (e, t, n) {
+        scr_system: [function (e, t, n) {//tag 设置界面
             "use strict";
             cc._RF.push(t, "00817ZNI7hJb4XVsFxczkla", "scr_system");
             cc.Class({
                 extends: cc.Component,
-                properties: {},
+                properties: {
+                    sprite: {  //关联编辑器
+                        type: cc.SpriteFrame,
+                        default: null,
+                    }
+                },
                 onLoad: function () {
-                    var t = ["一般", "快", "一般"], n = cc.find("Canvas/button/speed"), a = e("scr_data"), i = e("scr_effect");
-                    cc.find("Canvas/text/speed").getComponent("cc.Label").string = "你目前剧情（部分）播放速度为" + t[a.publicVar[6] || 2];
-                    n.on("touchstart", function () {
+                    var t = ["一般", "快", "一般"],
+                        n = cc.find("Canvas/button/speed"), a = e("scr_data"), i = e("scr_effect"),
+                        speedtext = cc.find("Canvas/text/speed");
+                    speedtext.getComponent("cc.Label").string = "你目前剧情（部分）播放速度为" + t[a.publicVar[6] || 2];
+                    let ex = cc.instantiate(n);//复制一个一模一样的按钮 1
+                    ex.setPosition(0, 459 - 200);//设置位置 往第一个按钮的下方移动200像素 2
+                    cc.find("Canvas/button").addChild(ex);  // 将按钮节点作为当前节点的子节点 3
+                    ex.getChildByName("New Label").getComponent("cc.Label").string = "倍 率";
+                    //获得按钮上的节点，用名字搜索子节点       子节点上有一个组件    修改组件的字符  4
+                    ex.color = cc.Color.RED;  //设置新节点的颜色
+                    //text.string = "测试按钮";//text.parent = ex;//text.color = cc.Color.RED;
+                    let exT = cc.instantiate(speedtext);//复制显示文字的label
+                    exT.setPosition(0, 557 - 200);// 如法炮制
+                    cc.find("Canvas/text").addChild(exT);
+                    exT.getComponent("cc.Label").string = "你当前拥有1x前进/探索速度\n（探索时会消耗对应倍数的精力\n但是奖励总量不变）";
+                    n.on("touchstart", function () {//添加触摸函数要最后加，不然复制的按钮也会有这个函数（也许吧）
                         a.publicVar[6] += 1;
                         a.publicVar[6] > 2 && (a.publicVar[6] = 1);
                         i.playText("Canvas/text/speed", "你目前剧情播放速度为" + t[a.publicVar[6]], 60);
                     }, n);
+                    ex.on("touchstart", function () {//添加触摸函数要最后加，不然复制的按钮也会有这个函数（也许吧）
+                        a.energyconsumetimes += 1;
+                        a.energyconsumetimes > 10 && (a.energyconsumetimes = 1);
+                        exT.getComponent("cc.Label").string = "你当前拥有 " + a.energyconsumetimes + "x 前进/探索速度\n（探索时会消耗对应倍数的精力\n但是奖励总量不变）";//todo
+                    }, ex);
                 }
             });
             cc._RF.pop();
